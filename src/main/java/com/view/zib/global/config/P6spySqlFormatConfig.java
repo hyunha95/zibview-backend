87238618b1fb29f554499878627e5d5c038bcea4
nohyunha <nohyunha95@gmail.com>
2024-05-26 15:13:55 +0900

    p6spy 설정

---
+++ src/main/java/com/view/zib/global/config/P6spySqlFormatConfig.java
@@ -0,0 +1,11 @@
+package com.view.zib.global.config;
+
+import com.p6spy.engine.common.P6Util;
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+
+public class P6spySqlFormatConfig implements MessageFormattingStrategy {
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
+        return "took " + elapsed + "ms|" + category + "|connection " + connectionId + "|query:" + P6Util.singleLine(sql);
+    }
+}
