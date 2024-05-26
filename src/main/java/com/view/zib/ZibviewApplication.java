934e99086b9efde9f61e0ce3dac318bf6fda5d1c
nohyunha <nohyunha95@gmail.com>
2024-05-26 15:15:04 +0900

    상용(prod) 환경에서는 ddl-auto=none만 가능하도록 설정

--- src/main/java/com/view/zib/ZibviewApplication.java
+++ src/main/java/com/view/zib/ZibviewApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZibviewApplication {
 
 	public static void main(String[] args) {
+		// Check if the environment is production and ddl-auto is not none
+		String profile = System.getenv("SPRING_PROFILES_ACTIVE");
+		String ddlType = System.getenv("DDL_AUTO");
+		if ("prod".equalsIgnoreCase(profile) && !"none".equalsIgnoreCase(ddlType)) {
+			throw new RuntimeException("You can't use ddl-auto other than none in production environment.");
+		}
+
 		SpringApplication.run(ZibviewApplication.class, args);
 	}
-
 }
