9ae8ec48f7022b39856eba26c4634bcdf527814a
nohyunha <nohyunha95@gmail.com>
2024-05-29 08:46:32 +0900

    native query 사용 시 mysql에만 존재하는 문법을 h2 데이터베이스에서 테스트할 수 없어서 test container 사용

---
+++ src/test/java/intg/com/view/zib/domain/post/controller/PostQueryControllerTest.java
@@ -0,0 +1,46 @@
+package com.view.zib.domain.post.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@SpringBootTest
+class PostQueryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Container
+    private MySQLContainer mySQLContainer = new MySQLContainer(DockerImageName.parse("mysql:8.0.36"));
+
+
+    /**
+     * This test uses a mysql native query
+     * 게시글을 가장 가까운 주소를 기준으로 조회한다.
+     */
+    @Test
+    void getPostByClosestAddress() throws Exception {
+        mockMvc.perform(get("/api/posts"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+    }
+
+
+}
