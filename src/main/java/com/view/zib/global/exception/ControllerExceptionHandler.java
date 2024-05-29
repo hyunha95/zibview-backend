0bc770208b48ff68b3104c77d4bf05894ebf49f6
nohyunha <nohyunha95@gmail.com>
2024-05-29 09:14:39 +0900

    핸들러 메소드 파라미터 유효성 검사 실패 테스트가 항상 동일한 결과를 얻기 위해서 invalidInputs 정렬 처리

--- src/main/java/com/view/zib/global/exception/ControllerExceptionHandler.java
+++ src/main/java/com/view/zib/global/exception/ControllerExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 @Slf4j
 @RestControllerAdvice
@@ -46,8 +47,11 @@ public class ControllerExceptionHandler {
     public ResponseEntity<Error> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         var invalidInputs = new ArrayList<Error.InvalidInput>();
         e.getBindingResult().getFieldErrors().forEach(fieldError -> {
-            invalidInputs.add(new Error.InvalidInput(fieldError.getField(), fieldError.getRejectedValue().getClass().getSimpleName(), fieldError.getDefaultMessage()));
+            invalidInputs.add(new Error.InvalidInput(fieldError.getField(), fieldError.getField().getClass().getSimpleName(), fieldError.getDefaultMessage()));
         });
+
+        invalidInputs.sort(Comparator.comparing(Error.InvalidInput::name));
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new Error("Invalid input", LocalDateTime.now(), invalidInputs));
