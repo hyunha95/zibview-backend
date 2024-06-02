9d91f71c6d1ce465ba02d07f520a2eb06f4fe5d0
nohyunha <nohyunha95@gmail.com>
2024-06-03 08:56:30 +0900

    단일 post 응답 dto 추가

--- src/main/java/com/view/zib/domain/post/controller/response/GetPostResponse.java
+++ src/main/java/com/view/zib/domain/post/controller/response/GetPostResponse.java
@@ -1,13 +1,105 @@
 package com.view.zib.domain.post.controller.response;
 
 import com.view.zib.domain.address.entity.Address;
+import com.view.zib.domain.building.enums.BuildingType;
+import com.view.zib.domain.post.entity.Comment;
+import com.view.zib.domain.post.entity.Post;
+import com.view.zib.domain.post.entity.SubPost;
+import com.view.zib.domain.storage.service.StorageService;
 
-public record GetPostResponse (
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.function.Function;
+
+public record GetPostResponse(
         double latitude,
-        double longitude
+        double longitude,
+        String buildingName,
+        String address,
+        BuildingType buildingType,
+        List<SubPostDto> subPosts
 ) {
 
-    public static GetPostResponse from(Address address) {
-        return new GetPostResponse(address.getLatitude(), address.getLongitude());
+    public static GetPostResponse from(Post post, StorageService storageService) {
+        Address address = post.getAddress();
+
+
+        List<SubPostDto> subPostDtos = post.getSubPostsNotDeletedOrderByIdDesc().stream()
+                .map(mapToSubPostDtos(storageService))
+                .toList();
+
+        return new GetPostResponse(
+                address.getLatitude(),
+                address.getLongitude(),
+                address.getBuildingName(),
+                address.getAddress(),
+                post.getBuildingType(),
+                subPostDtos
+        );
+    }
+
+    // SubPost Entity를 SubPostDto로 변환
+    private static Function<SubPost, SubPostDto> mapToSubPostDtos(StorageService storageService) {
+        return subPost -> {
+            ChronoUnit chronoUnit = ChronoUnit.MONTHS;
+            long residencePeriod = subPost.getContractStartDate().until(subPost.getContractEndDate(), chronoUnit);
+            List<String> imageUrls = subPost.getImages().stream()
+                    .map(storageService::generateImageUrl)
+                    .toList();
+
+            CommentDto latestComment = subPost.getLatestComment()
+                    .map(CommentDto::from)
+                    .orElse(null);
+
+            return new SubPostDto(
+                    subPost.getUser().getId(),
+                    subPost.getUser().getName(),
+                    subPost.getUser().getPictureUrl(),
+                    residencePeriod,
+                    chronoUnit,
+                    subPost.getCreatedAt(),
+                    subPost.getTitle(),
+                    subPost.getDescription(),
+                    imageUrls,
+                    subPost.getCommentCount(),
+                    latestComment
+            );
+        };
+    }
+
+    public record SubPostDto(
+            Long userId,
+            String userName,
+            String profileImageUrl,
+            long residencePeriod,
+            ChronoUnit residencePeriodUnit,
+            LocalDateTime createdAt,
+            String title,
+            String description,
+            List<String> imageUrls,
+            Long commentCount,
+            CommentDto latestComment
+    ) {
+    }
+
+    public record CommentDto(
+            Long userId,
+            String userName,
+            String profileImageUrl,
+            String comment,
+            LocalDateTime createdAt
+    ) {
+
+        // Comment Entity를 CommentDto로 변환
+        public static CommentDto from(Comment comment) {
+            return new CommentDto(
+                    comment.getUser().getId(),
+                    comment.getUser().getName(),
+                    comment.getUser().getPictureUrl(),
+                    comment.getComment(),
+                    comment.getCreatedAt()
+            );
+        }
     }
 }
