384b70d4a4495f5c0681cd2fed4f3aea1041f0eb
nohyunha <nohyunha95@gmail.com>
2024-06-03 08:54:57 +0900

    BuildingType enum에 description 추가

--- src/main/java/com/view/zib/domain/building/enums/BuildingType.java
+++ src/main/java/com/view/zib/domain/building/enums/BuildingType.java
@@ -1,6 +1,15 @@
 package com.view.zib.domain.building.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum BuildingType {
 
-    APARTMENT, OFFICETEL, HOUSE, VILLA
+    APARTMENT("아파트"), OFFICETEL("오피스텔"), HOUSE("주택"), VILLA("빌라");
+
+    private final String buildingTypeName;
+
+    BuildingType(String buildingTypeName) {
+        this.buildingTypeName = buildingTypeName;
+    }
 }
